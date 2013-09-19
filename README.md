790a6845cc56fa2a4640dc3110b271d6f8b0df26
Matt McManus <matt@mcmanus.io>
2013-09-19 13:51:01 -0400

    Merging ummon-cli and ummon-client into simply ummon

--- README.md
+++ README.md
@@ -1,6 +1,63 @@
-ummon
-=====
+# ummon
+## Javascript API Client & Command Line Tool
 
 [![NPM version](https://badge.fury.io/js/ummon.png)](http://badge.fury.io/js/ummon) [![Build Status](https://secure.travis-ci.org/punkave/ummon.png?branch=master)](http://travis-ci.org/punkave/ummon) [![Dependency Status](https://gemnasium.com/punkave/ummon.png)](https://gemnasium.com/punkave/ummon)
 
-**Work in Progess** The ummon api client
+The command line interface to the [ummon server](https://github.com/punkave/ummon-server). Since `ummon-server` only communicates over an HTTP API, you can install this module on any computer and as long as you have the url and a user and pass you can interact with it.
+
+# The Command Line Client
+
+## Installation
+
+```
+npm install -g ummon
+```
+
+## Usage
+
+```
+$ ummon --help
+
+Commands:
+
+   collection             Set the default settings for a particular collection
+   config                 Get the server status as well as control various aspects of the server
+   log                    Show recent log output. Filter logs by using --collection, --task or --run (only one!)
+   pulse                  Ping server status for a queue/worker numbers
+   queue                  Show or clear the queue
+   status                 Get the server status as well as control various aspects of the server
+   task                   Show and modify the configuration for a task
+   help <sub-command>     Show the --help for a specific command
+
+```
+
+## Default options for every command
+
+* `url`: The url where `ummon-server` is accepting connections
+* `retries`: The number of times
+* `timeout`: Time to wait
+* `username`
+* `password`
+
+## The `.ummonrc` file
+
+You can save all of the above settings in `~/.ummonrc` so you don't have to type them in every time.
+
+`$ cat ~/.ummonrc`
+```ini
+defaultEnv=development # The default environment. Recuded the need to type --env development
+
+[development] # As of now, environments are the only sections
+url=http://localhost:8888
+username=superusername
+password=science
+
+[production]
+url=http://111.111.111.111:8080
+username=nameOfUser
+password=superTopSecretPassword
+```
+
+Warning! Obviously if you save your password, it will be saved in clear text. Use appropriate caution
+
+# [Ummon Javascript Client](http://punkave.github.io/ummon/)
---
+++ cli-utils.js
@@ -0,0 +1,153 @@
+var util = require('util');
+var colors = require('colors');
+var _ = require('underscore');
+var rc = require('rc');
+var moment = require('moment');
+
+var l = console.log;
+
+
+/**
+ * Format an ummon task
+ *
+ * @param  {Task} task
+ */
+exports.formatTask = function(task) {
+  var heading = task.id.white;
+  if (task.enabled === false) { heading = '(disabled) '.red + heading}
+
+  var command = task.command;
+  command += (task.cwd)?' (in '+task.cwd +')':'';
+  l(heading + ': $ '.grey + command);
+
+  if (task.trigger) {
+    if (task.trigger.afterFailed) {
+      l('Trigger after Failed: '+task.trigger.afterFailed);
+    }
+    else {
+      var trigger = task.trigger.time || task.trigger.after || task.trigger;
+      l('Trigger: '+ trigger)
+    }
+  }
+
+  if (task.lastSuccessfulRun) {
+    l('Last successful run: ' + moment(task.lastSuccessfulRun).calendar())
+  }
+
+  if (task.recentExitCodes.length) {
+    // Group by exit codes, [0,0,1,1] becomes {'0':[0,0], '1':[1,1]}
+    var codes = _.groupBy(task.recentExitCodes);
+    // Count the zeros!
+    var countSuccess = (codes['0']) ? codes['0'].length : 0;
+    // No zeros / total exit codes * 100 === pecentage
+    var percentage = (countSuccess / task.recentExitCodes.length * 100);
+
+    var label = "Success rate (last 10): "
+
+    // This isn't pretty but it works for now
+    if (percentage < 20) {
+      l(label + percentage.toString().red + '%'.red);
+    } else if (percentage == 100) {
+      l(label + percentage.toString().green + '%'.green);
+    } else {
+      l(label + percentage.toString().yellow + '%'.yellow);
+    }
+  }
+
+  l('');
+}
+
+
+exports.formatJson = function(data) {
+  console.log(util.inspect(data, { showHidden: true, depth: null, colors: true }));
+}
+
+
+/**
+ * A simple helper to print key:value pairs
+ */
+exports.formatKeyVal = function(key, val) {
+  l(key + ': '+val);
+}
+
+
+exports.errorIfBadTaskID = function(task) {
+  if (task.indexOf('.') === -1) {
+    ifError(new Error('('+task+') is not a correct task id. Please use format: <collection>.<name>'));
+  }
+}
+
+/**
+ * Helper to print errors if there is an error
+ *
+ * @param  {Error} err [description]
+ * @return {[type]}     [description]
+ */
+exports.ifError = ifError = function(err) {
+  if (err) {
+    l('')
+    util.error('** ERROR **'.red);
+    if (err.statusCode) { util.error(' ('+err.statusCode+')') }
+    l('');
+    if (err.message) {
+      util.error(err.message.red);
+    } else {
+      util.error(err.red);
+    }
+    process.exit(1);
+  }
+}
+
+
+/**
+ * Helper to add all of the default options neccesary for each ummon cli
+ *
+ * @param {Object} program The commander object
+ */
+var addGlobalOptions = exports.addGlobalOptions = function(program) {
+  // Global Options
+  program
+    .option('--env <env>', 'The ummon environment to connect to (Automatically loaded from ~/.ummonrc if present)')
+    .option('-h, --url <url>', 'The url where the ummon server is running. (Automatically loaded from ~/.ummonrc if present)', 'http://localhost:8888')
+    .option('-u, --username <username>', 'Your username (Automatically loaded from ~/.ummonrc if present)')
+    .option('-p, --password <password>', 'Your password (Automatically loaded from ~/.ummonrc if present)')
+    .option('-r, --retry <count>', 'The amount of times to retry a failed connection', 3)
+    .option('-t, --timeout <seconds>', 'The amount of time to to wait between retries', 1);
+
+  return program;
+}
+
+
+/**
+ * Load configuration options from users .ummonrc file
+ *
+ * @param {Object} program The commander object
+ * @return {Object}         ummon configuration object
+ */
+var loadLocalConfig = exports.loadLocalConfig = function(program) {
+  var config = require('rc')('ummon', {}, {});
+
+  if (!program.env) {
+    program.env = config.defaultEnv;
+  }
+  // Bring env settings into the root
+  return _.extend(program, config[program.env])
+}
+
+
+/**
+ * Helper to quickly setup each task
+ *
+ * @param {Object} program The commander object
+ * @param  {Object} argv    Process argv
+ * @return {Object}         Fully modified commander object
+ */
+exports.parse = function(program, argv) {
+  program = addGlobalOptions(program);
+
+  program.parse(argv);
+
+  program = loadLocalConfig(program);
+
+  return program
+}
